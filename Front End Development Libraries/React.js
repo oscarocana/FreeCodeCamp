3a15a9fa1539f59b9ef8e9022cfc1f3e6127d12f
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-01-27 23:15:31 +0000

    Update React.js
    Pass an Array as Props

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -201,3 +201,31 @@ class Calendar extends React.Component {
     );
   }
 };
+
+//Pass an Array as Props
+
+const List = (props) => {
+  { /* Change code below this line */ }
+  return <p>{props.tasks.join(", ")}</p>
+  { /* Change code above this line */ }
+};
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>To Do Lists</h1>
+        <h2>Today </h2>
+        { /* Change code below this line */ }
+
+        <List tasks={["walk", "run", "swim"]}/>
+        <h2>Tomorrow </h2>
+        <List tasks={["code","Dance","Party"]}/>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
