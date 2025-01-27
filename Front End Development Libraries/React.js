c4b1d77d5ae8b0bc39f3a964f58d7f2f27079965
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-01-27 23:05:36 +0000

    Update React.js
    Write a React Component from Scratch

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -155,3 +155,21 @@ class TypesOfFood extends React.Component {
 // Change code below this line
 
 ReactDOM.render(<TypesOfFood />, document.getElementById("challenge-node"))
+
+//Write a React Component from Scratch
+
+// Change code below this line
+class MyComponent extends React.Component{
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <div>
+        <h1>My First React Component!</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById("challenge-node") )
