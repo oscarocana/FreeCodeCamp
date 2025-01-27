376069ee82f2f1215cafc4bc607cbdf7e7e87e37
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-01-27 22:52:30 +0000

    Update React.js
    Create a Component with Composition

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -25,3 +25,32 @@ class Calendar extends React.Component {
     );
   }
 };
+
+//Create a Component with Composition
+
+const ChildComponent = () => {
+  return (
+    <div>
+      <p>I am the child</p>
+    </div>
+  );
+};
+
+class ParentComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>I am the parent</h1>
+        { /* Change code below this line */ }
+          <ChildComponent />
+
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+};
+
+
