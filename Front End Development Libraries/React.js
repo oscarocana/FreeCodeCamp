ba43eda7fee7efd2b63de79b49a1af7c95f5c5dc
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-01-28 21:20:21 +0000

    Update React.js
    Access Props Using this.props

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -288,3 +288,38 @@ class ShoppingCart extends React.Component {
     return <Items />
   }
 };
+
+//Access Props Using this.props
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <Welcome name="Oscar" />
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+}
+
+class Welcome extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <p>Hello, <strong>{this.props.name}</strong>!</p>
+        { /* Change code above this line */ }
+      </div>
+    );
+  }
+}
+
