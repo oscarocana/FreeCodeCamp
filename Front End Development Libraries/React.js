81da59af41660eedfc89e5ba79714b45f1e2eb87
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-01-28 21:46:13 +0000

    Update React.js

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -457,3 +457,33 @@ class MyComponent extends React.Component {
   }
 };
 
+
+//Bind 'this' to a Class Method
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello"
+    };
+    // Change code below this line
+    this.handleClick = this.handleClick.bind(this)
+    // Change code above this line
+  }
+  handleClick() {
+    this.setState({
+      text: "You clicked!"
+    });
+  }
+  render() {
+    return (
+      <div>
+        { /* Change code below this line */ }
+        <button onClick={this.handleClick}>Click Me</button>
+        { /* Change code above this line */ }
+        <h1>{this.state.text}</h1>
+      </div>
+    );
+  }
+};
+
