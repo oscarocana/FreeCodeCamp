568b502983ad8aa833d55ff93cfd44b77695b985
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-01-30 23:45:15 +0000

    Update React.js
    Use the Lifecycle Method componentWillMount

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -742,3 +742,19 @@ class RenderInput extends React.Component {
     );
   }
 };
+
+//Use the Lifecycle Method componentWillMount
+
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentWillMount() {
+    // Change code below this line
+    console.log("shalom")
+    // Change code above this line
+  }
+  render() {
+    return <div />
+  }
+};
