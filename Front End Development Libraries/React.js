e85a042ad4bbd3c7a554aa58eb5f8a35510744af
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-02-02 22:40:11 +0000

    Update React.js
    Render React on the Server with renderToString

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -1287,3 +1287,18 @@ class MyComponent extends React.Component {
     );
   }
 }
+
+
+//Render React on the Server with renderToString
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <div/>
+  }
+};
+
+// Change code below this line
+ReactDOMServer.renderToString(<App />)
