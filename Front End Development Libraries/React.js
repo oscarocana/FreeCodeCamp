cfc95d18baa3dbfebb85c5b4426cc966554f8306
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-02-02 22:32:42 +0000

    Update React.js
    Give Sibling Elements a Unique Key Attribute

--- Front End Development Libraries/React.js
+++ Front End Development Libraries/React.js
@@ -1211,3 +1211,31 @@ class MyToDoList extends React.Component {
     );
   }
 }
+
+
+//Give Sibling Elements a Unique Key Attribute
+
+
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map((item, index) => (
+  <li key={index}>{item}</li>
+)); //Change this line
+
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
