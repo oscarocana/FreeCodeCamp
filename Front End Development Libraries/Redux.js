0f0a5b891ec53bfc68d02297421b84982dd8e7d8
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-02-03 22:23:26 +0000

    Update Redux.js
    Dispatch an Action Event

--- Front End Development Libraries/Redux.js
+++ Front End Development Libraries/Redux.js
@@ -37,3 +37,19 @@ const action = {
 const actionCreator = () => {
   return action
 }
+
+
+//Dispatch an Action Event
+
+const store = Redux.createStore(
+  (state = {login: false}) => state
+);
+
+const loginAction = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+// Dispatch the action here:
+store.dispatch(loginAction());
