282d5a67111259d90cd8243fe4a35c097d341d6a
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-02-03 22:39:53 +0000

    Update Redux.js

--- Front End Development Libraries/Redux.js
+++ Front End Development Libraries/Redux.js
@@ -81,3 +81,40 @@ const loginAction = () => {
     type: 'LOGIN'
   }
 };
+
+
+//Use a Switch Statement to Handle Multiple Actions
+
+
+const defaultState = {
+  authenticated: false
+};
+
+const authReducer = (state = defaultState, action) => {
+  // Change code below this line
+  switch(action.type){
+    case "LOGIN":
+     return state = {authenticated: true};
+      break
+  case "LOGOUT":
+     return {authenticated: false };
+      break
+  default:
+    return state
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(authReducer);
+
+const loginUser = () => {
+  return {
+    type: 'LOGIN'
+  }
+};
+
+const logoutUser = () => {
+  return {
+    type: 'LOGOUT'
+  }
+};
