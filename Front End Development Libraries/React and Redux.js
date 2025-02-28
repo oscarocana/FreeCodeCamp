1d90c04509311993572a61df60d4db6257ee0c35
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-02-28 21:02:52 +0000

    Update React and Redux.js
    Map Dispatch to Props

--- Front End Development Libraries/React and Redux.js
+++ Front End Development Libraries/React and Redux.js
@@ -186,3 +186,20 @@ const mapStateToProps = (state) => {
   const object = { messages: state }
   return object;
 }
+
+//Map Dispatch to Props
+
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// Change code below this line
+
+const mapDispatchToProps = (dispatch) => {
+  return { submitNewMessage : function(message) {
+    dispatch(addMessage(message))
+  } }
+}
