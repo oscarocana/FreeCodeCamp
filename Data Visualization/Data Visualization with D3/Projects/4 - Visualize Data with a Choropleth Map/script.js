2f1bc4d0650557d8a9b72a5f8e615829b083a1a6
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-04-01 21:59:27 +0100

    Update script.js
    basic JS layout

--- Data Visualization/Data Visualization with D3/Projects/4 - Visualize Data with a Choropleth Map/script.js
+++ Data Visualization/Data Visualization with D3/Projects/4 - Visualize Data with a Choropleth Map/script.js
@@ -1,1 +1,12 @@
+let countyURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"
+let educationLevelURL = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json"
 
+let canvas = d3.select("#canvas")
+
+let countyData = topojson.feature(d3.json(countyURL), d3.json(countyURL).objects.counties)
+let educationData = d3.json(educationLevelURL)
+
+let drawMap = () => {
+}
+
+console.log(countyData)
