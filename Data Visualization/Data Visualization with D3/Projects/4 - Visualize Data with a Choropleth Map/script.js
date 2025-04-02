b250c6394a87037ce95cffa8fa08c534e60dbca3
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-04-02 23:13:05 +0100

    Update script.js
    up tp user story 4 completed

--- Data Visualization/Data Visualization with D3/Projects/4 - Visualize Data with a Choropleth Map/script.js
+++ Data Visualization/Data Visualization with D3/Projects/4 - Visualize Data with a Choropleth Map/script.js
@@ -7,29 +7,54 @@ let countyData
 let educationData
 
 let drawMap = () => {
-}
 
+    canvas.selectAll("path")
+        .data(countyData)
+        .enter()
+        .append("path")
+        .attr("d", d3.geoPath())
+        .attr("class","county")
+        .attr("fill", (d) => {
+            let id = d["id"]
+            let county = educationData.find((d) => {
+                return d.fips === id
+            })
+            let percentage = county["bachelorsOrHigher"]
+            if (percentage <= 10){
+                return "red"
+            }else if (percentage <= 20){
+                return "orange"
+            }else if (percentage <= 30){
+                return "yellow"
+            }else if (percentage <= 40){
+                return "green"
+            }else{
+                return"darkgreen"
+            }
+        })
+}
+    
+    
 d3.json(countyURL).then(
     (data, error) => {
         if(error){
             console.log(error)
         }else{
-            countyData = topojson.feature(data, data.objects.counties)
+            countyData = topojson.feature(data, data.objects.counties).features
             console.log(countyData)
 
-            d3.json(educationData).then(
+            d3.json(educationLevelURL).then(
                 (data,error) => {
                     if(error){
                         console.log(error)
                     }else{
                         educationData = data
                         console.log(educationData)
+                        drawMap()
                     }
                 }
             )
         }
-        
-        
     }
 )
 
