cbdd8bdde2ebd0769c1e54a0f219c110084f0cdd
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-04-08 23:06:41 +0100

    Update script.js

--- Data Visualization/Data Visualization with D3/Projects/5 - Visualize Data with a Treemap Diagram/script.js
+++ Data Visualization/Data Visualization with D3/Projects/5 - Visualize Data with a Treemap Diagram/script.js
@@ -7,6 +7,24 @@ let tooltip
 
 let drawTreeMap = () => {
 
+    let hierarchy = d3.hierarchy(VideoGameData, (node) => {
+        return node.children
+    }).sum((node) => {
+        return node.value
+    }).sort((node1, node2) => {
+        return node2.value - node1.value
+    })
+
+    let createTreeMap = d3.treemap()
+                        .size([1000,1000])
+
+    createTreeMap(hierarchy)
+    console.log(hierarchy.leaves())
+
+    // canvas.selectAll("rect")
+    // .data(VideoGameData)
+    // .enter()
+    // .append("rect")
 }
 
 d3.json(VideoGameDataUrl).then(
