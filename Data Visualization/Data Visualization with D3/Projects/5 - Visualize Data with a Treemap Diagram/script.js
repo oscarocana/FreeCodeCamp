90c32baf68d27b91bdc7f1615ca82668107254d2
Oscar Ocana <92057829+oscarocana@users.noreply.github.com>
2025-04-08 23:38:17 +0100

    Update script.js
    US 4 completed will refactor with a switch later

--- Data Visualization/Data Visualization with D3/Projects/5 - Visualize Data with a Treemap Diagram/script.js
+++ Data Visualization/Data Visualization with D3/Projects/5 - Visualize Data with a Treemap Diagram/script.js
@@ -19,7 +19,7 @@ let drawTreeMap = () => {
                         .size([1000,1000])
 
     createTreeMap(hierarchy)
-    
+
     let leaves = hierarchy.leaves()
     console.log(hierarchy.leaves())
 
@@ -30,6 +30,44 @@ let drawTreeMap = () => {
 
     tile.append("rect")
         .attr("class","tile")
+        .attr("fill",(d) => {
+            let category = d["data"]["category"]
+            if(category==="Wii"){
+                return "#8B8B8B"
+            }else if(category==="NES"){
+                return "red"
+            }else if(category==="GB"){
+                return "#8A2BE2"
+            }else if(category==="DS"){
+                return "#FFD700"
+            }else if(category==="X360"){
+                "#A2C837"
+            }else if(category==="PS3"){
+                return "#686868"
+            }else if(category==="PS2"){
+                return "#003087"
+            }else if(category==="SNES"){
+                return "#800080"
+            }else if(category==="GBA"){
+                return "#8A2BE2"
+            }else if(category==="PS4"){
+                return "#1C1C1C"
+            }else if(category==="3DS"){
+                return "#FF4500"
+            }else if(category==="N64"){
+                return "#000080"
+            }else if(category==="PS"){
+                return "#808080"
+            }else if(category==="XB"){
+                return "#107C10"
+            }else if(category==="PC"){
+                return "#4682B4"
+            }else if(category==="PSP"){
+                return "#2F4F4F"
+            }else{
+                return "#52B043"
+            }
+        })
 }
 
 d3.json(VideoGameDataUrl).then(
